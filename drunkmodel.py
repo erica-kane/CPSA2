26986d15cb2db72221709877d3028bafeec40a2a
ericamcg <71267518+ericamcg@users.noreply.github.com>
2020-11-05 17:27:58 +0000

    Update drunkmodel.py

--- drunkmodel.py
+++ drunkmodel.py
@@ -18,13 +18,42 @@ matplotlib.pyplot.show()
 
 housenumbers = [float(i) for i in list(range(10, 260, 10))]
 houses = []
+houses_dict = {}
 
 for x, row in enumerate(plan):
     for y, value in enumerate(row):
         if value >= 10.0 and value <= 250.0:
-            houses.append((x, y, value))
+            houses.append([int(x), int(y), int(value)])
 
 
+# Setting 'house numbers' as the keys of the dictionary, and empty lists as values
+for row in houses:
+    houses_dict[row[2]] = []
+    
+# Appending tuples of x and y values of the houses to the value lists in the dictionary 
+for key, value in houses_dict.items():
+    for row in houses:
+        if row[2] == key:
+            value.append((row[0], row[1]))
 
 
+# Check the dictionary by comparing length of each list of coordinates
+# to how many house number 30's there were in houses and plan
+for key, value in houses_dict.items():
+    print(len(value))
 
+practice = []
+for row in houses:
+    if row[2] == 30:
+        practice.append(row[2])
+
+len(practice)       
+
+practicetwo = []
+
+for row in plan:
+    for value in row:
+        if value == 30:
+            practicetwo.append(value)
+
+len(practicetwo)
