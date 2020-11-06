27dd0033f5b38e5b9e5afd65778f19a8b532f8c9
ericamcg <71267518+ericamcg@users.noreply.github.com>
2020-11-06 11:09:55 +0000

    Update drunkmodel.py

--- drunkmodel.py
+++ drunkmodel.py
@@ -30,12 +30,15 @@ for x, row in enumerate(plan):
 for row in houses:
     houses_dict[row[2]] = []
     
-# Appending tuples of x and y values of the houses to the value lists in the dictionary 
-for key, value in houses_dict.items():
-    for row in houses:
-        if row[2] == key:
-            value.append((row[0], row[1]))
+# Appending tuples of x and y values of the houses to the value lists in the dictionary
+for row in houses:
+    houses_dict[row[2]].append((row[0], row[1]))
 
+## Below is first attempt of the above code, also works but is less efficient
+# for key, value in houses_dict.items():
+#     for row in houses:
+#         if row[2] == key:
+#             houses_dict[key].append((row[0], row[1]))
 
 # Check the dictionary by comparing length of each list of coordinates
 # to how many house number 30's there were in houses and plan
@@ -46,14 +49,14 @@ practice = []
 for row in houses:
     if row[2] == 30:
         practice.append(row[2])
-
 len(practice)       
 
 practicetwo = []
-
 for row in plan:
     for value in row:
-        if value == 30:
+        if value == 240:
             practicetwo.append(value)
-
 len(practicetwo)
+
+# All show 121 coordinates per house, which suggests an 11x11 area
+
