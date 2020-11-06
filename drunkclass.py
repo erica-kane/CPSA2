be3453649236178d64a07d8fa0da80518fb2e2fe
ericamcg <71267518+ericamcg@users.noreply.github.com>
2020-11-06 16:31:08 +0000

    updates

--- drunkclass.py
+++ drunkclass.py
@@ -1,5 +1,10 @@
 
 
 class House():
-    def __init__(self, number):
-        self.number = number 
+    def __init__(self, number, coords): 
+        self.number = number
+        self.coords = coords
+
+class Pub():
+    def __init__(self, coords):
+        self.coords = coords
--- drunkmodel.py
+++ drunkmodel.py
@@ -1,7 +1,7 @@
 import csv
 import matplotlib
-import matplotlib.pyplot
-
+import matplotlib.pyplot as plt 
+import drunkclass
 
 plan = []
 f = open('drunk.plan.txt', newline = '')
@@ -12,27 +12,29 @@ f.close()
 len(plan)
 len(plan[0])
 
-matplotlib.pyplot.imshow(plan)
-matplotlib.pyplot.show()
+plt.imshow(plan)
+plt.show()
 
-
-housenumbers = [float(i) for i in list(range(10, 260, 10))]
+houses_info = []
+houses_info_dict = {}
+pub_info = []
 houses = []
-houses_dict = {}
 
+
+# Getting coordinates of each house point from the 'plan'
 for x, row in enumerate(plan):
     for y, value in enumerate(row):
         if value >= 10.0 and value <= 250.0:
-            houses.append([int(x), int(y), int(value)])
+            houses_info.append([int(x), int(y), int(value)])
 
 
 # Setting 'house numbers' as the keys of the dictionary, and empty lists as values
-for row in houses:
-    houses_dict[row[2]] = []
+for row in houses_info:
+    houses_info_dict[row[2]] = []
     
 # Appending tuples of x and y values of the houses to the value lists in the dictionary
-for row in houses:
-    houses_dict[row[2]].append((row[0], row[1]))
+for row in houses_info:
+    houses_info_dict[row[2]].append((row[0], row[1]))
 
 ## Below is first attempt of the above code, also works but is less efficient
 # for key, value in houses_dict.items():
@@ -42,11 +44,11 @@ for row in houses:
 
 # Check the dictionary by comparing length of each list of coordinates
 # to how many house number 30's there were in houses and plan
-for key, value in houses_dict.items():
+for key, value in houses_info_dict.items():
     print(len(value))
 
 practice = []
-for row in houses:
+for row in houses_info:
     if row[2] == 30:
         practice.append(row[2])
 len(practice)       
@@ -59,4 +61,26 @@ for row in plan:
 len(practicetwo)
 
 # All show 121 coordinates per house, which suggests an 11x11 area
+
+
+# Getting coordinates of pub point from the 'plan' and appending to a list of tuples 
+for x, row in enumerate(plan):
+    for y, value in enumerate(row):
+        if value == 1.0:
+            pub_info.append((int(x), int(y)))
+len(pub_info)
+# length 441, suggesting 21x21 grid 
+
+
+# Make the houses 
+for key, value in houses_info_dict.items():
+    number = key
+    coords = value   
+    houses.append(drunkclass.House(number, coords))
+
+
+for house in houses:
+    for coord in house.coords:
+        plt.scatter(coord[0], coord[1], s=0.1, c="b")
+
 
