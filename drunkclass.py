e6cbffdcdf2660f0e96cdf5b33ba4c7ed7832fce
ericamcg <71267518+ericamcg@users.noreply.github.com>
2020-11-11 17:31:29 +0000

    drunk walk

--- drunkclass.py
+++ drunkclass.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt 
+import random
 
 class Building():
     def __init__(self, coords): 
@@ -26,11 +27,11 @@ class Building():
         self.height = maxy - miny
 
         doory = miny
-        doorx = minx + (self.width/2)
+        doorx = minx + int(self.width/2)
         self.door = (doorx, doory)
 
         centrex = doorx
-        centrey = miny + (self.height/2)
+        centrey = miny + int(self.height/2)
         self.centre = (centrex, centrey)
 
 
@@ -43,7 +44,7 @@ class House(Building):
         house_outline = plt.Rectangle(self.bl, self.width, self.height, fill=False)
         ax.add_patch(house_outline)
         ax.text(self.tr[0], self.tr[1], str(self.number))
-        plt.scatter(self.door[0], self.door[1], marker='s', s=1)
+        plt.scatter(self.door[0], self.door[1], marker='s', s=1, c='k')
         
 
 
@@ -54,7 +55,50 @@ class Pub(Building):
         ax.text(self.tr[0], self.tr[1], 'Pub', color = 'r')
         plt.scatter(self.door[0], self.door[1], marker='s', s=1, c='r')
 
+
+
 class Drunk():
-    def __init__(self, plan, home):
+    def __init__(self, x, y, drunks, plan, home):
+        self.x = x
+        self.y = y
+        self.drunks = drunks
         self.plan = plan 
         self.home = home
+        self.y_max = len(plan) - 1
+        self.x_max = len(plan[0]) - 1
+
+
+    def walk(self):
+        if random.random() < 0.5:
+            nexty = (self.y + 1)
+        else:
+            nexty = (self.y - 1)
+
+        if random.random() < 0.5:
+            nextx = (self.x + 1)
+        else:
+            nextx = (self.x - 1)
+
+
+        if nextx < 0:
+            nextx = 0
+        elif nextx > self.x_max:
+            nextx = self.x_max
+        
+        if nexty < 0:
+            nexty = 0
+        elif nexty > self.y_max:
+            nexty = self.y_max
+
+
+        building_value = self.plan[nexty][nextx]
+        if building_value == 0:
+            self.x = nextx
+            self.y = nexty
+
+
+        if self.x == self.home.door[0] and self.y == self.home.door[1]:
+            self.drunks.remove(self)
+
+    def draw(self):
+        plt.scatter(self.x, self.y)
--- drunkmodel.py
+++ drunkmodel.py
@@ -1,5 +1,6 @@
 import csv
 import matplotlib.pyplot as plt 
+import matplotlib.animation 
 import drunkclass
 
 plan = []
@@ -81,7 +82,7 @@ pub = drunkclass.Pub(pub_info)
 
 # Create the drunks
 for house in houses:
-    drunks.append(drunkclass.Drunk(plan, house))
+    drunks.append(drunkclass.Drunk(pub.door[0], pub.door[1], drunks, plan, house))
 
 # Testing points 
 for house in houses:
@@ -96,12 +97,36 @@ plt.scatter(pub.tr[0], pub.tr[1], s=1, c='r')
 # https://stackoverflow.com/questions/37435369/matplotlib-how-to-draw-a-rectangle-on-image
 fig, ax = plt.subplots()
 
-for house in houses:
-    house.draw(fig, ax)
+# for house in houses:
+#     house.draw(fig, ax)
 
-pub.draw(fig, ax)
+# pub.draw(fig, ax)
+
+# drunks[0].draw()
+
+# for num in range(1_000):
+#     drunks[0].walk()
+    
+# drunks[0].draw()
 
 plt.axis('square')
 ax.autoscale_view()
+
+def update(frame_number):
+    
+    fig.clear()  
+
+    for house in houses:
+        house.draw(fig, ax)
+    
+    pub.draw(fig, ax)
+
+    for drunk in drunks:
+        drunk.walk()
+        drunk.draw()
+
+
+animation = matplotlib.animation.FuncAnimation(fig, update, interval = 1, repeat = False, frames = 10000)
+plt.show()
 
 
